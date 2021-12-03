1f5e3b43e74adcbd2499e786df639528e1e4f74c
ziebam <ziebamichal@tutanota.com>
2021-12-03 19:03:20 +0100

    Refactor solution for day 3 part 1

--- day3/day3.nim
+++ day3/day3.nim
@@ -1,5 +1,4 @@
 import std/strutils
-import std/tables
 
 proc loadData(path: string): seq[string] =
     let data: File = open(path)
@@ -12,26 +11,20 @@ proc loadData(path: string): seq[string] =
 proc partOne(): int =
     let binNumbers: seq[string] = loadData("day3/day3.txt")
 
-    var oneBits = initTable[int, int]()
+    var bitCounts = newSeq[int](len(binNumbers[0]))
     for binNumber in binNumbers:
         for index, bit in binNumber:
-            if $bit == "1":
-                if not oneBits.hasKey(index):
-                    oneBits[index] = 0
-                else:
-                    oneBits[index] += 1
+            if bit == '1':
+                inc bitCounts[index]
 
-    var gammaRate = "000000000000"
-    for key in oneBits.keys:
-        if oneBits[key] > 500:
-            gammaRate[key] = '1'
-
-    var epsilonRate = ""
-    for bit in gammaRate:
-        if bit == '0':
-            epsilonRate.add('1')
-        else:
+    var gammaRate, epsilonRate = ""
+    for bit in bitCounts:
+        if bit > (len(binNumbers) / 2).int:
+            gammaRate.add('1')
             epsilonRate.add('0')
+        else:
+            gammaRate.add('0')
+            epsilonRate.add('1')
 
     return parseBinInt(gammaRate) * parseBinInt(epsilonRate)
 
