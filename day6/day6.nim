e7ab4759f9f8199cafaa619fad256c4c98e92e6a
ziebam <ziebamichal@tutanota.com>
2021-12-06 23:35:05 +0100

    Add solution for day 6 part 2

--- day6/day6.nim
+++ day6/day6.nim
@@ -24,9 +24,23 @@ proc calculateDay(lanternfishSeq: var seq[Lanternfish]) =
         
 
 var lanternfishSeq = loadData("day6/day6.txt")
-echo "Initial: ", lanternfishSeq.len
+# echo "Initial: ", lanternfishSeq.len
+# for i in 1..80:
+#     let before = lanternfishSeq.len
+#     calculateDay(lanternfishSeq)
+#     let after = lanternfishSeq.len
+#     echo "After day ", i, ": ", lanternfishSeq.len, " (+ ", after - before, ")"
+
+type
+    Calculation = object
+        afterDay: int
+        amount: int
+
+var calculations: seq[Calculation] = @[Calculation(afterDay: 0, amount: 300)]
 for i in 1..80:
-    let before = lanternfishSeq.len
     calculateDay(lanternfishSeq)
-    let after = lanternfishSeq.len
-    echo "After day ", i, ": ", lanternfishSeq.len, " (+ ", after - before, ")"
+    calculations.add(Calculation(afterDay: i, amount: lanternfishSeq.len))
+for i in countup(81, 256, 1):
+    calculations.add(Calculation(afterDay: i, amount: calculations[i - 9].amount + calculations[i - 7].amount))
+
+echo calculations[256]
