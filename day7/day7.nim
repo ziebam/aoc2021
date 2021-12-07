bd0203b042d90e06957411603d492c5e43c62353
ziebam <ziebamichal@tutanota.com>
2021-12-07 17:03:12 +0100

    Add solution for day 7 part 2
    This commit comes so quickly after the last one because I forgot
    to commit at first and wrote solutions for both parts before that.

--- day7/day7.nim
+++ day7/day7.nim
@@ -16,6 +16,20 @@ proc partOne(positions: seq[int]): int =
         results.add(fuel)
     result = min(results)
 
+proc partTwo(positions: seq[int]): int =
+    let lowestPosition = min(positions)
+    let highestPosition = max(positions)
+
+    var results: seq[int]
+    for i in lowestPosition..highestPosition:
+        var fuel: int = 0
+        for position in positions:
+            for j in 1..abs(position - i):
+                fuel += j
+        results.add(fuel)
+    result = min(results)
+
 let positions = loadData("day7/day7.txt")
 
 echo "Part one: ", partOne(positions)
+echo "Part two: ", partTwo(positions)
