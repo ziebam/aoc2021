387044c43526da532a663dde98a2551715181084
ziebam <ziebamichal@tutanota.com>
2021-12-10 22:30:35 +0100

    Refactor solutions for day 6
    Properly split the code into procedures and explained the method
    for calculating the amount of lanternfish after n days while
    avoiding exponential growth of the time it takes to execute
    the program.

--- day6/day6.nim
+++ day6/day6.nim
@@ -4,13 +4,19 @@ type
     Lanternfish = object
         timer: int
 
+type
+    Day = object
+        amount: int
+
 proc loadData(path: string): seq[Lanternfish] =
     let allLanternfish: seq[string] = readFile(path).split(",")
 
     for lanternfish in allLanternfish:
         result.add(Lanternfish(timer: parseInt(lanternfish)))
 
-proc calculateDay(lanternfishSeq: var seq[Lanternfish]) =
+proc nextDay(lanternfishSeq: seq[Lanternfish]): seq[Lanternfish] =
+    var lanternfishSeq = lanternfishSeq
+
     var howManyCreated = 0
     for lanternfish in lanternfishSeq.mitems:
         if lanternfish.timer == 0:
@@ -21,26 +27,33 @@ proc calculateDay(lanternfishSeq: var seq[Lanternfish]) =
 
     for i in 1..howManyCreated:
         lanternfishSeq.add(Lanternfish(timer: 8))
-        
+
+    return lanternfishSeq
+
+proc partOneAndTwo(lanternfishSeq: seq[Lanternfish], days: int): int =
+    var lanternfishSeq = lanternfishSeq
+
+    var daysPassed: seq[Day] = @[]
+    daysPassed.add(Day(amount: lanternfishSeq.len))
+
+    for i in 1..8:
+        lanternfishSeq = nextDay(lanternfishSeq)
+        daysPassed.add(Day(amount: lanternfishSeq.len))
+
+    #[
+        After brute-forcing the solution for part one and playing around with
+        the values, I noticed that the amount of lanternfish on a given
+        day `n` is equal to the sum of the amount of lanternfish on days
+        `n-9` and `n-7`. Therefore, we only need to calculate the initial
+        amount and the amounts after the first 8 days to easily calculate
+        the rest.
+    ]#
+    for n in 9..days:
+        let amountAfterNextDay = daysPassed[n - 9].amount + daysPassed[n - 7].amount
+        daysPassed.add(Day(amount: amountAfterNextDay))
+    
+    result = daysPassed[days].amount
 
 var lanternfishSeq = loadData("day6/day6.txt")
-# echo "Initial: ", lanternfishSeq.len
-# for i in 1..80:
-#     let before = lanternfishSeq.len
-#     calculateDay(lanternfishSeq)
-#     let after = lanternfishSeq.len
-#     echo "After day ", i, ": ", lanternfishSeq.len, " (+ ", after - before, ")"
-
-type
-    Calculation = object
-        afterDay: int
-        amount: int
-
-var calculations: seq[Calculation] = @[Calculation(afterDay: 0, amount: 300)]
-for i in 1..80:
-    calculateDay(lanternfishSeq)
-    calculations.add(Calculation(afterDay: i, amount: lanternfishSeq.len))
-for i in countup(81, 256, 1):
-    calculations.add(Calculation(afterDay: i, amount: calculations[i - 9].amount + calculations[i - 7].amount))
-
-echo calculations[256]
+echo "Part one: ", partOneAndTwo(lanternfishSeq, 80)
+echo "Part two: ", partOneAndTwo(lanternfishSeq, 256)
