a935558e7c54b913e9193813b5ab7fc3ebc3108f
ziebam <ziebamichal@tutanota.com>
2021-12-10 18:19:19 +0100

    Add solution for day 10 part 2
    Merged it with part one while I was at it.

--- day10/day10.nim
+++ day10/day10.nim
@@ -1,3 +1,4 @@
+import std/algorithm
 import std/tables
 
 proc loadData(path: string): seq[string] =
@@ -8,22 +9,59 @@ proc loadData(path: string): seq[string] =
     while readLine(lines, line):
         result.add(line)
 
-const Scores = {')': 3, ']': 57, '}': 1197, '>': 25137}.toTable
+const ScoresPartOne = {')': 3, ']': 57, '}': 1197, '>': 25137}.toTable
+const ScoresPartTwo = {')': 1, ']': 2, '}': 3, '>': 4}.toTable
 const BracketPairs = {'(': ')', '[': ']', '{': '}', '<': '>'}.toTable
 
-proc partOne(lines: seq[string]): int =
-    for line in lines:
+proc dropCorrupted(lines: seq[string]): seq[string] =
+    var incompleteLines: seq[string] = lines
+
+    var indicesToRemove: seq[int] = @[]
+    for idx, line in lines:
         var openedBlocks: seq[char] = @[]
         for c in line:
             if BracketPairs.hasKey(c):
                 openedBlocks.add(c)
             else:
                 if c != BracketPairs[openedBlocks[^1]]:
-                    result += Scores[c]
+                    indicesToRemove.add(idx)
                     break
                 else:
                     discard openedBlocks.pop()
+    
+    for idx in countdown(indicesToRemove.len - 1, 0, 1):
+        incompleteLines.delete(indicesToRemove[idx])
+
+    return incompleteLines
+
+proc partOneAndTwo(lines: seq[string], partOne: static bool = true): int =
+    when not partOne:
+        var scores: seq[int] = @[]
+
+    for line in lines:
+        var openedBlocks: seq[char] = @[]
+        for c in line:
+            if BracketPairs.hasKey(c):
+                openedBlocks.add(c)
+            else:
+                when partOne:
+                    if c != BracketPairs[openedBlocks[^1]]:
+                        result += ScoresPartOne[c]
+                        break
+                
+                discard openedBlocks.pop()
+
+        when not partOne:
+            var score = 0
+            for openingChar in reversed(openedBlocks):
+                score = score * 5 + ScoresPartTwo[BracketPairs[openingChar]]
+            scores.add(score)
+
+    when not partOne:
+        result = sorted(scores)[scores.len div 2]
 
 let lines = loadData("day10/day10.txt")
+let incompleteLines = dropCorrupted(lines)
 
-echo partOne(lines)
+echo "Part one: ", partOneAndTwo(lines)
+echo "Part two: ", partOneAndTwo(incompleteLines, false)
