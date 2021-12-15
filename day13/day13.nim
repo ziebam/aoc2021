5e3f795aa4410f58023c8b60db2c06c106608ef2
ziebam <ziebamichal@tutanota.com>
2021-12-15 10:42:03 +0100

    Add solution for day 13 part 2

--- day13/day13.nim
+++ day13/day13.nim
@@ -36,52 +36,73 @@ proc loadData(path: string): Sheet =
 
     inc result.width
     inc result.height
-        
-proc partOne(sheet: Sheet): int =
+
+proc fold(sheet: Sheet, axis: string): Sheet =
     var newSheet: Sheet
-    for fold in sheet.folds:
-        if fold.axis == "y":
-            newSheet.width = sheet.width
-            newSheet.height = sheet.height div 2
+    if axis == "y":
+        newSheet.width = sheet.width
+        newSheet.height = sheet.height div 2
 
-            for dot in sheet.dots:
-                # Dot in the top half.
-                if dot.y < newSheet.height:
-                    if newSheet.dots.contains((x: dot.x, y: dot.y)):
-                        continue
-                    else:
-                        newSheet.dots.add(dot)
-                # Dot in the bottom half.
+        for dot in sheet.dots:
+            # Dot in the top half.
+            if dot.y < newSheet.height:
+                if newSheet.dots.contains((x: dot.x, y: dot.y)):
+                    continue
                 else:
-                    let newY = dot.y - 2 * (dot.y - newSheet.height)
-                    let newDot = (x: dot.x, y: newY)
-                    if newSheet.dots.contains(newDot):
-                        continue
-                    else:
-                        newSheet.dots.add(newDot)
-        else:
-            newSheet.width = sheet.width div 2
-            newSheet.height = sheet.height
-
-            for dot in sheet.dots:
-                # Dot in the left half.
-                if dot.x < newSheet.width:
-                    if newSheet.dots.contains((x: dot.x, y: dot.y)):
-                        continue
-                    else:
-                        newSheet.dots.add(dot)
-                # Dot in the right half.
+                    newSheet.dots.add(dot)
+            # Dot in the bottom half.
+            else:
+                let newY = dot.y - 2 * (dot.y - newSheet.height)
+                let newDot = (x: dot.x, y: newY)
+                if newSheet.dots.contains(newDot):
+                    continue
                 else:
-                    let newX = dot.x - 2 * (dot.x - newSheet.width)
-                    let newDot = (x: newX, y: dot.y)
-                    if newSheet.dots.contains(newDot):
-                        continue
-                    else:
-                        newSheet.dots.add(newDot)
+                    newSheet.dots.add(newDot)
+    else:
+        newSheet.width = sheet.width div 2
+        newSheet.height = sheet.height
 
-    result = newSheet.dots.len
+        for dot in sheet.dots:
+            # Dot in the left half.
+            if dot.x < newSheet.width:
+                if newSheet.dots.contains((x: dot.x, y: dot.y)):
+                    continue
+                else:
+                    newSheet.dots.add(dot)
+            # Dot in the right half.
+            else:
+                let newX = dot.x - 2 * (dot.x - newSheet.width)
+                let newDot = (x: newX, y: dot.y)
+                if newSheet.dots.contains(newDot):
+                    continue
+                else:
+                    newSheet.dots.add(newDot)
 
+    return newSheet
+
+proc printSheet(sheet: Sheet) =
+    for y in 0..sheet.height - 1:
+        var row = ""
+        for x in 0..sheet.width - 1:
+            if sheet.dots.contains((x: x, y: y)):
+                row.add('#')
+            else:
+                row.add(' ')
+        echo row
+
+proc partOneAndTwo(sheet: Sheet, partOne: static bool = true): int =
+    var newSheet = sheet
+
+    when partOne:
+        newSheet = fold(newSheet, sheet.folds[0].axis)
+        result = newSheet.dots.len
+    else:
+        for fold in sheet.folds:
+            newSheet = fold(newSheet, fold.axis)
+        result = newSheet.dots.len
+        printSheet(newSheet)
 
 let sheet = loadData("day13/day13.txt")
 
-echo "Part one: ", partOne(sheet)
+echo "Part one: ", partOneAndTwo(sheet)
+echo "Part two: ", partOneAndTwo(sheet, false)
